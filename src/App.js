db2aaaf4543584391cbd8b0a4eee737f29b32b3e
vishal_kasa <v-vishalkasa@microsoft.com>
2022-04-27 15:47:22 +0530

    added new

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ import OrgChart from './tree';
                     <div style={{height: '100%'}}>
 
                         <OrgChart nodes={[
-                            { id: 1,name: "Mini Thomas", title: "Principal Engineering Manager", img: "https://cdn.balkan.app/shared/2.jpg" },
+                            { id: 1,name: "Mini Thomas", title: "Principal Engineering Manager", img: "https://cdn.balkan.app/shared/2.jpg" ,tags: ['orange']},
                             { id: 2, pid: 1,name: "Sneha Naik", title: "Engineering Lead", img: "https://cdn.balkan.app/shared/3.jpg" },
-                            { id: 3, pid: 1,name: "Ankit Mudgal", title: "Senior Engineering Lead", img: "https://cdn.balkan.app/shared/3.jpg" },
+                            { id: 3, pid: 1,name: "Ankit Mudgal",title: "Senior Engineering Lead", img: "https://cdn.balkan.app/shared/3.jpg" },
                             { id: 4, pid: 1,name: "Apoorv Ghildhiyal", title: "Engineering Lead", img: "https://cdn.balkan.app/shared/8.jpg" },
                             { id: 5, pid: 1,name: "Shakil Shaikh", title: "Lead Software Engineer", img: "https://cdn.balkan.app/shared/3.jpg" },
                             { id: 6, pid: 1,name: "Ashwini Ingle", title: "Senior Software Engineer", img: "https://cdn.balkan.app/shared/5.jpg" },
--- src/tree.js
+++ src/tree.js
@@ -5,6 +5,7 @@ import OrgChart from '@balkangraph/orgchart.js';
             constructor(props) {
                 super(props);
                 this.divRef = React.createRef();
+                
             }
 
             shouldComponentUpdate() {
@@ -12,20 +13,54 @@ import OrgChart from '@balkangraph/orgchart.js';
             }
 
             componentDidMount() {
+                OrgChart.templates.diva = Object.assign({}, OrgChart.templates.diva);
+                OrgChart.templates.diva.editFormHeaderColor = '#282534';
                 this.chart = new OrgChart (this.divRef.current , {
+                    readOnly: true,
                     nodes: this.props.nodes,
-
+                    mouseScrool: OrgChart.action.none,
+                    template: "diva",
                     nodeBinding: {
                         field_0: "name",
                         field_1: "title",
                         img_0: "img"
+                    },
+                    tags: {
+                        orange: {
+                            template: 'diva'
+                        }
+                    },
+                    editForm: {
+                        generateElementsFromFields: false,
+            elements: [
+                { type: 'textbox', label: 'Full Name', binding: 'name'},
+                { type: 'textbox', label: 'Designation', binding: 'title'}        
+            ],
+                        buttons:  {
+                            share: null,
+                            pdf: null,
+                            edit :null
+                        },
+                        
                     }
+            
                 });
+                // this.chart.on('click', function (sender, args) {
+                //     if (args.node.min) {
+                //         sender.maximize(args.node.id);
+                //     }
+                //     else {
+                //         sender.minimize(args.node.id);
+                //     }
+                //     return false;
+                // });
             }
 
             render() {
                 return (
-                    <div id="tree" ref={this.divRef}></div>
+                    <div id="tree" ref={this.divRef}>
+                    </div>
+                    
                 );
             }
         }
